7abba6ec8bb2dc45f555f65ac4a9db93a676aeb5
Andre <andredelima92@gmail.com>
2022-02-16 19:33:50 -0300

    state concept

---
+++ src/components/Counter.jsx
@@ -0,0 +1,19 @@
+import React, { useState } from "react";
+
+function Counter() {
+  const [count, setCount] = useState(0);
+  function increment() {
+    setCount(count + 1);
+  }
+
+  return (
+    <div>
+      <h2>{count}</h2>
+      <button type="button" onClick={increment}>
+        Increment
+      </button>
+    </div>
+  );
+}
+
+export default Counter;
