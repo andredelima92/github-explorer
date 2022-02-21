7bafad9d387afe3e62849186bc69ea032b75f305
Andre <andredelima92@gmail.com>
2022-02-21 20:59:45 -0300

    added external api call

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,20 +1,26 @@
+import { useEffect, useState } from "react";
 import RepositoryItem from "./RepositoryItem";
-
-const repository = {
-  name: "unform",
-  description: "Forms in React",
-  link: "https://github.com/unform/unform",
-};
+import "../styles/repositories.scss";
 
 export const RepositoryList = () => {
+  const [repositories, setRepositories] = useState([]);
+
+  function getRepositories() {
+    fetch("https://api.github.com/orgs/rocketseat/repos")
+      .then((response) => response.json())
+      .then((data) => setRepositories(data));
+  }
+
+  useEffect(getRepositories, []);
+
   return (
     <section className="repository-list">
       <h1>Lista de repositórios</h1>
 
       <ul>
-        <RepositoryItem repository={repository} />
-        <RepositoryItem repository={repository} />
-        <RepositoryItem repository={repository} />
+        {repositories.map((repository) => (
+          <RepositoryItem key={repository.name} repository={repository} />
+        ))}
       </ul>
     </section>
   );
